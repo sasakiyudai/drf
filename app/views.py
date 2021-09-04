958a60bb12597b0d37aa2f1248782b517dd8cb3d
sasakiyudai <yudai14142@gmail.com>
2021-09-04 16:55:03 +0900

    before auth

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.db.models import query
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
+
+class PostView(generics.ListAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
